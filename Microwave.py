b733d162953a86383d181f20d7ce1edf96d885f9
Shademoon <55607744+shademoonn@users.noreply.github.com>
2021-09-06 16:42:39 -0400

    Update Microwave.py

--- Microwave.py
+++ Microwave.py
@@ -3,11 +3,16 @@ Microwave = {'a': 'v.h.h', 'b': 'v.h.m', 'c': 'v.h.V', 'd': 'v.h.M', 'e': 'v.H.v
              'o': 'h.H.v', 'p': 'h.H.h', 'q': 'h.H.m', 'r': 'h.H.V', 's': 'h.H.M', 't': 'm.h.v', 'u': 'm.h.h',
              'v': 'm.h.m', 'w': 'm.h.V', 'x': 'm.h.M', 'y': 'm.H.v', 'z': 'm.H.h', ' ': ''}
 
-string = input("Enter your sentence: ")
-if type(string) != str:
-    print("Please enter a string")
+while True:
+    string = input("Enter your sentence (q to quit): ")
+    if type(string) != str:
+        print("Please enter a string")
 
-else:
-    letters = list(string.lower())
-    for letter in letters:
-        print(Microwave[letter], end='-')
+    if string == 'q':
+        break
+
+    else:
+        letters = list(string.lower())
+        for letter in letters:
+            print(Microwave[letter], end='-')
+    print("")
